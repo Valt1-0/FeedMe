6593c7fcb691792e4bee51c31c665c30c0cbe5be
kyliansup <kylian.chesneau@supinfo.com>
2023-03-24 23:23:22 +0100

    add RecipeItem.kt

--- app/src/main/java/com/example/feedme/ui/components/recipeItem/RecipeItem.kt
+++ app/src/main/java/com/example/feedme/ui/components/recipeItem/RecipeItem.kt
@@ -3,15 +3,25 @@ package com.example.feedme.ui.components.recipeItem
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import com.example.feedme.database.model.Recipe
 import com.example.feedme.navigation.Screen
+import com.example.feedme.ui.components.ConvertRating
+import com.example.feedme.ui.components.LoadImageFromUrl
 import com.example.feedme.ui.components.RecipeCard
+import java.text.SimpleDateFormat
+import java.util.*
+import com.example.feedme.domain.RecipeWithFavorite as RecipeWithFavorite
 
 /*fun listItem() {
     val recipeItem = listOf("pomme", "banane", "boeuf")
@@ -38,36 +48,70 @@ LazyColumn {
     }
 }*/
 
+fun convertIngredientStringToList(ingredientString: String):List<String>{
+    return ingredientString.split(",")
+}
 
 @Composable
-fun recipeItemsList(recipes: List<Screen.RecipeDetails>, OnClick : (Int, Boolean) -> Unit){
-    val itemsList = listOf("ingrédient1", "ingédient2", "ingédient3", "ingédient4", "ingédient5")
+fun recipeItemsList(
+    recipe: RecipeWithFavorite
+){
+    val sdf = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+    val ingredientString = "pomme, myonnaise, boeuf, poisson"
+    val itemsListString = convertIngredientStringToList(ingredientString)
 
     Column(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(7.dp)
     ){
+        Box(modifier = Modifier.fillMaxWidth()){
+            LoadImageFromUrl(LocalContext.current,recipe.featuredImage)
+
+        Text(
+            text = recipe.title + "-" + recipe.publisher,
+            modifier = Modifier.padding(top = 5.dp),
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp
+        )
+
         Row (
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 20.dp, top = 10.dp, bottom = 10.dp),
-            horizontalArrangement = Arrangement.Start
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
         ){
             Text(
-                text = "Liste d'ingrédient",
-                fontSize = 40.sp,
-                fontWeight = FontWeight.ExtraBold
+                text = sdf.format(recipe.dateUpdated),
+                color = Color(0xFFAAAAAA),
+                modifier = Modifier.padding(top = 5.dp),
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp
             )
-        }
-        LazyRow(
-            modifier = Modifier
-                .fillMaxWidth()
-                .size(width = 380.dp, height = 230.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
-        ){
-            itemsListIndexed(recipes) {
-                index, recipe -> RecipeCard(recipe, OnClick)
+            Spacer(modifier = Modifier.width(4.dp))
+            Surface(
+                modifier = Modifier.size(26.dp, 23.dp),
+                shape = CircleShape,
+                color = Color(0xFFEEEEEE)
+            ){
+                Text(
+                    color = Color.Black,
+                    text = ConvertRating(recipe.rating).toString(),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 12.sp,
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .weight(1f)
+                )
             }
+            Row() {
+                Text(
+                    text = "Liste d'ingrédient :",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            }
+        }
         }
     }
 }
-
