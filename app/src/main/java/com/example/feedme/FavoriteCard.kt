b71f3ac89665f101b174c92305493547b6c77251
Dim <44736220+MitryDim@users.noreply.github.com>
2023-03-30 15:32:09 +0200

    Update card favorties with .clip(RoundedCornerShape(8.dp)

--- app/src/main/java/com/example/feedme/FavoriteCard.kt
+++ app/src/main/java/com/example/feedme/FavoriteCard.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
@@ -59,7 +61,7 @@ fun FavoriteCard(
             LoadImageFromUrl(
                 LocalContext.current,
                 recipe.featuredImage,
-                modifier = Modifier.size(150.dp)
+                modifier = Modifier.size(150.dp).clip(RoundedCornerShape(8.dp))
             )
             Column(
                 horizontalAlignment = Alignment.Start,
